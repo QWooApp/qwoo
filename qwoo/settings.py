04957d4283e457ecb3c86189d19868485e3e1d45
mentix02 <manan.yadav02@gmail.com>
2021-03-03 01:58:05 +0530

    Configured custom auth model.

--- qwoo/settings.py
+++ qwoo/settings.py
@@ -56,6 +56,8 @@ DATABASES = {
     }
 }
 
+AUTH_USER_MODEL = 'user.User'
+
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
---
+++ user/models.py
@@ -0,0 +1,27 @@
+from typing import List
+
+from django.db import models
+from django.contrib.auth.models import AbstractUser, UserManager as BaseUserManager
+
+
+class UserManager(BaseUserManager):
+    def active(self) -> models.QuerySet:
+        """ Helper filter to fetch active users. """
+        return self.filter(is_active=True)
+
+
+class User(AbstractUser):
+
+    """ Django's contrib.auth model mostly suffices for project. """
+
+    objects = UserManager()
+
+    # Optional field for providing OTP based auth in future
+    phone_number = models.CharField(max_length=11, null=True, blank=True)
+
+    class Meta:
+        ordering = ('-date_joined',)
+        indexes: List[models.Index] = [
+            models.Index(fields=('active',)),
+            models.Index(fields=('active', 'username')),
+        ]
