b0c4321fbc1354b5f182eeca19e8ff2925b110dc
mentix02 <manan.yadav02@gmail.com>
2021-03-05 06:45:02 +0530

    Optimized query with selected_related.

--- blog/views.py
+++ blog/views.py
@@ -22,6 +22,7 @@ class RecentPublicPostListAPIView(ListAPIView):
         if qs:
             return qs
         else:
-            qs = Post.objects.filter(user__privacy=User.PUBLIC)[:20]
+            q_filter = {'user__privacy': User.PUBLIC}
+            qs = Post.objects.filter(**q_filter).select_related('user')[:20]
             cache.set('recent_public_posts', qs)
             return qs
