124f8f30363cc8c991e6730e9308d25f36651b0d
mentix02 <manan.yadav02@gmail.com>
2021-03-06 15:31:46 +0530

    Removed caching.

--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,9 @@
-from django.core.cache import cache
-
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.generics import ListAPIView, CreateAPIView
 
 from blog.models import Post
 from user.models import User
 from blog.serializers import PostListSerializer, PostCreateSerializer
-
-CNT = 0
 
 
 class CreatePostAPIView(CreateAPIView):
@@ -17,20 +13,9 @@ class CreatePostAPIView(CreateAPIView):
 
 
 class RecentPublicPostListAPIView(ListAPIView):
-    cache_results = None
     serializer_class = PostListSerializer
 
     def get_queryset(self):
-        if self.cache_results:
-            return self.cache_results
-        qs = cache.get('recent_public_posts')
-        if qs:
-            self.cache_results = qs
-            return qs
-        else:
-            qs = Post.objects.filter(user__privacy=User.PUBLIC).select_related('user')[
-                :20
-            ]
-            cache.set('recent_public_posts', qs)
-            self.cache_results = qs
-            return qs
+        return Post.objects.filter(user__privacy=User.PUBLIC).select_related('user')[
+            :20
+        ]
