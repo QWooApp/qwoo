cccfa2ffbb9586da09d467f119af345ee7ed6794
mentix02 <manan.yadav02@gmail.com>
2021-03-07 08:58:40 +0530

    Registered signals to app config.

--- blog/apps.py
+++ blog/apps.py
@@ -3,3 +3,7 @@ from django.apps import AppConfig
 
 class BlogConfig(AppConfig):
     name = 'blog'
+
+    def ready(self):
+        # noinspection PyUnresolvedReferences
+        from blog.signals import increment_count_fields, decrement_count_fields
