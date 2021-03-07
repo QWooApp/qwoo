5861788123252e3c36f99e60a4e05d1d0c679590
mentix02 <manan.yadav02@gmail.com>
2021-03-07 10:55:02 +0530

    Added views for looking at hearters.

--- heart/urls.py
+++ heart/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 
-from heart.views import HeartCreateAPIView, HeartDeleteAPIView
+from heart.views import HeartCreateAPIView, HeartDeleteAPIView, PostHeartUserListAPIView
 
 app_name = 'heart'
 
 urlpatterns = [
     path('create/', HeartCreateAPIView.as_view(), name='create'),
     path('delete/<slug:post_id>/', HeartDeleteAPIView.as_view(), name='create'),
+    path(
+        'hearters/<slug:post_id>/', PostHeartUserListAPIView.as_view(), name='hearters'
+    ),
 ]
--- heart/views.py
+++ heart/views.py
@@ -1,16 +1,26 @@
 from django.shortcuts import get_object_or_404
 
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.generics import CreateAPIView, DestroyAPIView
+from rest_framework.generics import ListAPIView, CreateAPIView, DestroyAPIView
 
+from user.models import User
 from heart.models import Heart
 from heart.serializers import HeartSerializer
+from user.serializers import UserListSerializer
 
 
 class HeartCreateAPIView(CreateAPIView):
     queryset = Heart.objects.all()
     serializer_class = HeartSerializer
     permission_classes = (IsAuthenticated,)
+
+
+class PostHeartUserListAPIView(ListAPIView):
+    serializer_class = UserListSerializer
+
+    def get_queryset(self):
+        post_id = self.kwargs.get('post_id')
+        return User.objects.filter(hearts__post_id=post_id)
 
 
 class HeartDeleteAPIView(DestroyAPIView):
